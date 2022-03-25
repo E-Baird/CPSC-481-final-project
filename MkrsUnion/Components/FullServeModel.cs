c525552f702b244e1d26e4c4b5a3f9621e4face9
Fysal <fysal.beauferris1@ucalgary.ca>
2022-03-25 16:13:39 -0600

    added basic form component

---
+++ MkrsUnion/Components/FullServeModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MkrsUnion.Components
+{
+	public class FullServeModel
+	{
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(8)]
+        public string UCID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Comments { get; set; }
+    }
+}
+
