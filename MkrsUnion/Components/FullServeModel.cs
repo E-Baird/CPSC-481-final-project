ae20eb27ed12994550ef7ba62353f12e55e573f1
Fysal <fysal.beauferris1@ucalgary.ca>
2022-03-25 18:56:10 -0600

    added browse field/scroll option

--- MkrsUnion/Components/FullServeModel.cs
+++ MkrsUnion/Components/FullServeModel.cs
@@ -3,8 +3,18 @@ using System.ComponentModel.DataAnnotations;
 
 namespace MkrsUnion.Components
 {
-	public class FullServeModel
-	{
+    public class FullServeModel
+    {
+        public ColourCategory Colour { get; set; }
+
+        public int Copies { get; set; }
+
+        public int DimensionX { get; set; }
+
+        public int DimensionY { get; set; }
+
+        public int DimensionZ { get; set; }
+
         [Required]
         public string Name { get; set; }
 
