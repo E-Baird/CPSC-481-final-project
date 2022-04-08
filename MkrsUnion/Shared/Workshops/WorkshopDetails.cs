47194c297ca11c7ce56c9e6323c987727246c294
E-Baird <emilyiris.baird@gmail.com>
2022-04-08 12:36:18 -0600

    implement ability to filter on category or weekday

--- MkrsUnion/Shared/Workshops/WorkshopDetails.cs
+++ MkrsUnion/Shared/Workshops/WorkshopDetails.cs
@@ -10,16 +10,18 @@ namespace MkrsUnion.Shared.Workshops {
         public string date { get; set; }
         public string time { get; set; }
         public string imgSrc { get; set; }
+        public string weekday { get; set; }
         public int imgNum { get; set; }
         public string description { get; set; }
-        public string categories { get; set; }
+        public List<string> categories { get; set; }
 
         // constructor
         // 'description', 'imgSrc', 'imgNum' parameters are optional
-        public WorkshopDetails(string title, string date, string time, string categories = "", string imgSrc = "", int imgNum = -1, string description = "")
+        public WorkshopDetails(string title, string date, string weekday, string time, List<string> categories, string imgSrc = "", int imgNum = -1, string description = "")
         {
             this.title = title;
             this.date = date;
+            this.weekday = weekday;
             this.time = time;
             this.imgSrc = imgSrc;
             this.description = description;
