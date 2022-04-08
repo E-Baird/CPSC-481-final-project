780092215c85ff8517e5f93be1f656702c17cdeb
E-Baird <emilyiris.baird@gmail.com>
2022-04-08 11:04:09 -0600

    move workshop details into their own class

---
+++ MkrsUnion/Shared/Workshops/WorkshopDetails.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MkrsUnion.Shared.Workshops { 
+    public class WorkshopDetails
+    {
+        // attributes
+        public string title { get; set; }
+        public string date { get; set; }
+        public string time { get; set; }
+        public string imgSrc { get; set; }
+        public int imgNum { get; set; }
+        public string description { get; set; }
+        public string categories { get; set; }
+
+        // constructor
+        // 'description', 'imgSrc', 'imgNum' parameters are optional
+        public WorkshopDetails(string title, string date, string time, string categories = "", string imgSrc = "", int imgNum = -1, string description = "")
+        {
+            this.title = title;
+            this.date = date;
+            this.time = time;
+            this.imgSrc = imgSrc;
+            this.description = description;
+            this.categories = categories;
+        }
+    }
+}
+
